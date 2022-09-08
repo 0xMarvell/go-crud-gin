949faa40b6b2c8d4cde0dbef4ecb7efc8b81b7d1
0xMarvell <rokemarvellous@gmail.com>
2022-09-08 16:24:06 +0100

    added routes package for handling routing

---
+++ pkg/routes/postRoutes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/0xMarvell/simple-blog-posts/pkg/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterBlogRoutes(r *gin.Engine) {
+	r.POST("/api/v1/blogposts", controllers.CreatePost)
+}
