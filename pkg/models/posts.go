30a113ae4385782e02e9e73758f964ffae386d8e
0xMarvell <rokemarvellous@gmail.com>
2022-09-08 14:00:08 +0100

    created database model for blog posts

---
+++ pkg/models/posts.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+// Post is the database model for blog posts.
+type Post struct {
+	gorm.Model
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
