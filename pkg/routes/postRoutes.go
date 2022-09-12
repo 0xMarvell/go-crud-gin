527285b02169027c7526973037971e8aa3cf5c6c
0xMarvell <rokemarvellous@gmail.com>
2022-09-12 15:53:31 +0100

    updated URL routes

--- pkg/routes/postRoutes.go
+++ pkg/routes/postRoutes.go
@@ -7,9 +7,9 @@ import (
 
 // RegisterBlogRoutes handles all routing for the API.
 func RegisterBlogRoutes(r *gin.Engine) {
-	r.POST("/api/v1/blogposts", controllers.CreatePost)
-	r.GET("/api/v1/blogposts", controllers.GetPosts)
-	r.GET("/api/v1/blogposts/:id", controllers.GetPost)
-	r.PUT("/api/v1/blogposts/:id", controllers.UpdatePost)
-	r.DELETE("/api/v1/blogposts/:id", controllers.DeletePost)
+	r.POST("/api/v1/posts", controllers.CreatePost)
+	r.GET("/api/v1/posts", controllers.GetPosts)
+	r.GET("/api/v1/posts/:id", controllers.GetPost)
+	r.PUT("/api/v1/posts/:id", controllers.UpdatePost)
+	r.DELETE("/api/v1/posts/:id", controllers.DeletePost)
 }
