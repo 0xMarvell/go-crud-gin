775bc4110f72fc3c73d376177aaaf86dc82de27d
0xMarvell <rokemarvellous@gmail.com>
2022-09-12 17:23:07 +0100

    testing

--- pkg/config/loadEnv.go
+++ pkg/config/loadEnv.go
@@ -1,12 +1,11 @@
 package config
 
 import (
-	"github.com/0xMarvell/simple-blog-posts/pkg/utils"
 	"github.com/joho/godotenv"
 )
 
 // LoadEnv loads the provided environment variables.
 func LoadEnv() {
-	err := godotenv.Load()
-	utils.CheckErr("error loading environment variables: ", err)
+	godotenv.Load()
+	// utils.CheckErr("error loading environment variables: ", err)
 }
